eb9ef41f1f1d9eaf0db132a4d03b378a2cc82125
juma <raisben7@gmail.com>
2024-02-01 20:03:50 -0800

    Changed the course and its parts into a single JavaScript object and optimized some parts of the code using the map function and a loop.

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,70 @@
 const Header = (props) => {
+  console.log(props)
   return (
     <div>
-      <h1><strong>{props.course}</strong></h1>
+      <h1><strong>{props.course.name}</strong></h1>
     </div>
   )
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
-       <ol>
-        <li>{props.parts[0].name} <strong>{props.parts[0].exercises}</strong></li>
-        <li>{props.parts[1].name} <strong>{props.parts[1].exercises}</strong></li>
-        <li>{props.parts[2].name} <strong>{props.parts[2].exercises}</strong></li>
-      </ol> 
+       <ul>
+        <font size ='5'>
+          {
+            props.course.parts.map((item) => (
+              <li>{item.name} <strong>{item.exercises}</strong></li>
+            ))
+          }
+        </font>
+      </ul> 
     </div>
   )
 }
 
 const Total = (props) => {
+  console.log(props)
+  let total = 0 
+  props.course.parts.forEach((item) => total += item.exercises)
   return (
     <div>
       <p>
-        The total of all exercises is {' '}
-        <strong>
-          {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-        </strong>
+        <font size = '5'>
+          The total of all exercises is {' '}
+          <strong> {total} </strong>
+        </font>
       </p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack Application Development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+  const course = {
+    name: 'Half Stack Application Development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
 
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
 
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
       <Header course = {course} />
-      <Content parts = {parts} />
-      <Total parts = {parts}  />
+      <Content course = {course} />
+      <Total course = {course}  />
 
     </div>
   )
